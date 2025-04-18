c15ad1e5db821df834a05ae958ddc4d3f0a6c188
LwBane <emiliepessoarodrigues@gmail.com>
2025-04-17 21:23:55 -0300

    Adicionei exercício ecomoeda

---
+++ JS/JS(Rafa)/EcoMoeda/ecomoeda.js
@@ -0,0 +1,17 @@
+
+function quantidade(){
+
+    let brinquedos 
+
+    brinquedos = Number(prompt("Quantos brinquedos você levou?:"))
+
+if(brinquedos ==1){
+    alert("Você receberá 1 moeda!")
+}else if(brinquedos ==2){
+    alert("Você receberá 2 moedas!")
+}else if(brinquedos >=3){
+    alert("Você receberá 3 moedas!")
+}else{
+    alert("Você não receberá moedas!")
+}
+}
