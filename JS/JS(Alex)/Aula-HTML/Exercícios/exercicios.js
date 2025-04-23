5558051d59231d9753648c8ad7dd0619ea1ec6ee
LwBane <emiliepessoarodrigues@gmail.com>
2025-04-22 21:54:45 -0300

    Organizando exercício

--- JS/JS(Alex)/Aula-HTML/Exercícios/exercicios.js
+++ JS/JS(Alex)/Aula-HTML/Exercícios/exercicios.js
@@ -1,17 +1,3 @@
-function exercicio1(){
-    let num1 = Number(prompt("Digite o primeiro número:"))
-    let num2 = Number(prompt("Digite o segundo número:"))
-    let soma = num1 + num2
-    alert("A soma dos dois números é: " + soma)
-}
-
-function exercicio2(){
-    let num1 = Number(prompt("Digite o primeiro número:"))
-    let num2 = Number(prompt("Digite o segundo número:"))
-    let media = (num1 + num2) / 2
-    alert(`A média dos dois números é:  + ${media}`)
-}
-
 function ex1(){
     let nomeUsuario = prompt("Digite seu nome: ")
     alert(`Olá ${nomeUsuario}, seja bem-vindo(a)!`)
