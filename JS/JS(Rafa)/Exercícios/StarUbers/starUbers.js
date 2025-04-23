3ded4ae5ac3b479e28c8b374fc92e6d673dd408a
LwBane <emiliepessoarodrigues@gmail.com>
2025-04-23 18:32:21 -0300

    modificações no código

--- JS/JS(Rafa)/Exercícios/StarUbers/starUbers.js
+++ JS/JS(Rafa)/Exercícios/StarUbers/starUbers.js
@@ -6,5 +6,28 @@ function calcularTempo(){
     let tempoHoras = distanciaCorrida / velocidadeLuz
     let tempoSegundos = tempoHoras * 3600 // tempo em segundos
 
-    alert(`O tempo da corrida é: ${tempoSegundos} segundos`)
+
+    if (tempoSegundos >= 31536000) {
+        let tempoAnos = tempoSegundos / 31536000;
+        alert(`Em anos: ${tempoAnos} anos\nEm ${tempoSegundos} segundos`);
+    }
+    else if (tempoSegundos >= 2592000) {
+        let tempoMeses = tempoSegundos / 2592000;
+        alert(`Em meses: ${tempoMeses} meses\nEm ${tempoSegundos} segundos`);
+    }
+    else if (tempoSegundos >= 86400) {
+        let tempoDias = tempoSegundos / 86400;
+        alert(`Em dias: ${tempoDias} dias\nEm ${tempoSegundos} segundos`);
+    }
+    else if (tempoSegundos >= 3600) {
+        let tempoHoras = tempoSegundos / 3600;
+        alert(`Em horas: ${tempoHoras} horas\nEm ${tempoSegundos} segundos`);
+    }
+    else if (tempoSegundos >= 60) {
+        let tempoMinutos = tempoSegundos / 60;
+        alert(`Em minutos: ${tempoMinutos} minutos\nEm ${tempoSegundos} segundos`);
+    }
+    else {
+        alert(`O tempo da corrida é: ${tempoSegundos} segundos`);
+    }
 }
