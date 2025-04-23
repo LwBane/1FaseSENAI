434f2be2c8bd8ee0a69d6dc43a9005a343e0c765
LwBane <emiliepessoarodrigues@gmail.com>
2025-04-23 19:13:06 -0300

    Modificações

--- JS/JS(Rafa)/Exercícios/StarUbers/starUbers.js
+++ JS/JS(Rafa)/Exercícios/StarUbers/starUbers.js
@@ -8,24 +8,24 @@ function calcularTempo(){
 
 
     if (tempoSegundos >= 31536000) {
-        let tempoAnos = tempoSegundos / 31536000;
-        alert(`Em anos: ${tempoAnos} anos\nEm ${tempoSegundos} segundos`);
+        let tempoAnos = tempoSegundos / 31536000
+        alert(`Em ${tempoAnos} anos\nEm ${tempoSegundos} segundos`);
     }
     else if (tempoSegundos >= 2592000) {
-        let tempoMeses = tempoSegundos / 2592000;
-        alert(`Em meses: ${tempoMeses} meses\nEm ${tempoSegundos} segundos`);
+        let tempoMeses = tempoSegundos / 2592000
+        alert(`Em ${tempoMeses} meses\nEm ${tempoSegundos} segundos`);
     }
     else if (tempoSegundos >= 86400) {
-        let tempoDias = tempoSegundos / 86400;
-        alert(`Em dias: ${tempoDias} dias\nEm ${tempoSegundos} segundos`);
+        let tempoDias = tempoSegundos / 86400
+        alert(`Em ${tempoDias} dias\nEm ${tempoSegundos} segundos`);
     }
     else if (tempoSegundos >= 3600) {
-        let tempoHoras = tempoSegundos / 3600;
-        alert(`Em horas: ${tempoHoras} horas\nEm ${tempoSegundos} segundos`);
+        let tempoHoras = tempoSegundos / 3600
+        alert(`Em ${tempoHoras} horas\nEm ${tempoSegundos} segundos`);
     }
     else if (tempoSegundos >= 60) {
-        let tempoMinutos = tempoSegundos / 60;
-        alert(`Em minutos: ${tempoMinutos} minutos\nEm ${tempoSegundos} segundos`);
+        let tempoMinutos = tempoSegundos / 60
+        alert(`Em ${tempoMinutos} minutos\nEm ${tempoSegundos} segundos`);
     }
     else {
         alert(`O tempo da corrida é: ${tempoSegundos} segundos`);
