f2dbfca6eb7aecb63b3bf0a8914c01f047716c4b
LwBane <emiliepessoarodrigues@gmail.com>
2025-04-23 20:15:46 -0300

    Adição de exercícios

--- JS/JS(Alex)/Aula-HTML/Exercícios/Lista 2/exercicios.js
+++ JS/JS(Alex)/Aula-HTML/Exercícios/Lista 2/exercicios.js
@@ -1,6 +1,21 @@
 function ex1(){
     let mensagem = prompt("Digite um texto: ")
-    
+    let simbolos = {
+        "a": "@",
+        "e": "#",
+        "i": "!",
+        "o": "$",
+        "u": "*"
+    }
+    let mensagemCodificada = ""
+    for(let letra of mensagem){
+        if(simbolos[letra]){
+            mensagemCodificada += simbolos[letra]
+        }else{
+            mensagemCodificada += letra
+        }
+    }
+    alert(`Mensagem codificada: ${mensagemCodificada}`)
 }
 
 function ex2(){
@@ -12,9 +27,12 @@ function ex3(){
 }
 
 function ex4(){
-    let num1 = Number(prompt("Digite um número:"))
+    let escolha = prompt("Escolha par ou ímpar:")
+    let numJogador = Number(prompt("Digite um número:"))
 
-    if(num1 % 2 == 0){
+    let soma = numJogador 
+
+    if(soma % 2 == 0){
         alert("O número é par.")
     }
     else{
@@ -23,42 +41,5 @@ function ex4(){
 }
 
 function ex5(){
-    let num1 = Number(prompt("Digite o primeiro número:"))
-    let num2 = Number(prompt("Digite o segundo número:"))
-    let num3 = Number(prompt("Digite o terceiro número:"))
-    let media = (num1 + num2 + num3) / 3
-    alert(`A média dos três números é:  + ${media}`)
-}
-
-function ex6(){
-    let peso = Number(prompt("Digite o seu peso(kg):"))
-    let altura = Number(prompt("Digite a sua altura(m):"))
-    let imc = peso / altura ** 2
-    alert(`Seu IMC é: ${imc}`)
-
-    if(imc < 18.5){
-        alert("Você está abaixo do peso.")
-    }
-    else if(imc >= 18.5 && imc <= 24.9){
-        alert("Você está com o peso normal.")
-    }
-    else if(imc >= 25 && imc <= 29.9){
-        alert("Você está com sobrepeso.")
-    }
-    else{
-        alert("Você está obeso.")
-    }}
-
-function ex7(){
-    let num1 = Number(prompt("Digite um número:"))
-    alert(`Opa, esse exercício ainda está em construção, pedimos desculpas!`)
-    }
-
-function ex8(){
-    let ano = Number(prompt("Digite um ano:"))
-    if((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)){
-        alert(`${ano} é um ano bissexto.`)
-    }
-    else{
-        alert(`${ano} não é um ano bissexto.`)
-    }}
+    
+  }
