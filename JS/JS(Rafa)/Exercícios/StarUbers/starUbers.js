0ec444e40d160b45a665d74e9e00d5552acab18e
LwBane <emiliepessoarodrigues@gmail.com>
2025-04-23 17:58:48 -0300

    Modificações no arquivo

--- JS/JS(Rafa)/Exercícios/StarUbers/starUbers.js
+++ JS/JS(Rafa)/Exercícios/StarUbers/starUbers.js
@@ -1,11 +1,10 @@
 function calcularTempo(){
 
-    let distanciaCorrida, calculoTempo
-    let velocidadeLuz = 300000 //km/s
-    
-    calculoTempo = distanciaCorrida / velocidadeLuz 
+    let distanciaCorrida = Number(prompt("Qual a distância da corrida?(km):"))
 
-    distanciaCorrida = Number(prompt("Qual a distância da corrida?(km):"))
+    let velocidadeLuz = 300000 * 3600 // velocidade da luz em km/h
+    let tempoHoras = distanciaCorrida / velocidadeLuz
+    let tempoSegundos = tempoHoras * 3600 // tempo em segundos
 
-    alert("O tempo da corrida é: " + calculoTempo + " segundos")
+    alert(`O tempo da corrida é: ${tempoSegundos} segundos`)
 }
