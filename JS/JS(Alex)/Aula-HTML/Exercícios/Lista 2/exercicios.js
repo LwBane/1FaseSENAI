686334e1f52fb4862641844af5e9a3407ebf1b98
LwBane <emiliepessoarodrigues@gmail.com>
2025-04-23 21:53:57 -0300

    Arrumando exercícios

--- JS/JS(Alex)/Aula-HTML/Exercícios/Lista 2/exercicios.js
+++ JS/JS(Alex)/Aula-HTML/Exercícios/Lista 2/exercicios.js
@@ -20,26 +20,62 @@ function ex1(){
 
 function ex2(){
     let nome = prompt("Digite seu nome:")
+
+    let min = 1 
+    let max = 4
+
+    let numero = Math.floor(Math.random() * (max - min) + min) 
+
+    let frase1 = "Capitão América"
+    let frase2 = "Homem de Ferro"
+    let frase3 = "Thor"
+
+    if(numero === 1){
+        alert(nome + ", você é o " + frase1)
+        console.log(nome + ", você é o " + frase1)
+    }
+    else if(numero === 2){
+        alert(nome + ", você é o " + frase2)
+    }
+    else if(numero === 3){
+        alert(nome + ", você é o " + frase3)
 }
+}
+
+//math .floor = arredonda p baixo 
+// 
+
 
 function ex3(){
     let signo = prompt("Digite seu signo:")
 }
 
 function ex4(){
-    let escolha = prompt("Escolha par ou ímpar:")
+    let escolha = prompt("Escolha par ou ímpar:").toLowerCase()
     let numJogador = Number(prompt("Digite um número:"))
 
-    let soma = numJogador 
+    let numMaquina = Math.floor(Math.random() * 10)  // math.floor = arredondar números com virgula // math.random = gera um número aleatório entre 0 e 1 // * 10 = gera um número entre 0 e 10 // +1 = para não gerar o número 0
+    let soma = numJogador + numMaquina
 
-    if(soma % 2 == 0){
-        alert("O número é par.")
-    }
-    else{
-        alert("O número é ímpar.")
+    alert(`A máquina escolheu o número: ${numMaquina}, você escolheu o número: ${numJogador}`)
+    alert(`A soma dos números é: ${soma}`)
+
+    if(soma % 2 == 0 && escolha === "par"){
+        alert("Você ganhou!")
+    } 
+    else if(soma % 2 == 0 && escolha === "impar"){
+        alert("Você perdeu!")
+    } 
+    else if(soma % 2 != 0 && escolha === "impar"){
+        alert("Você ganhou")
+    } 
+    else if(soma % 2 != 0 && escolha === "par"){
+        alert("Você perdeu!")
     }
 }
+// math.ceil = arredonda números pra cima 
+
 
 function ex5(){
-    
+
   }
