058adee901ccdc5eae99304e791ca5c0e7d8dadd
LwBane <emiliepessoarodrigues@gmail.com>
2025-05-05 19:54:49 -0300

    Mostrando outras formas pra fazer

--- JS/JS(Alex)/Exercícios/Lista While/Lista pair - while/senha.js
+++ JS/JS(Alex)/Exercícios/Lista While/Lista pair - while/senha.js
@@ -1,11 +1,12 @@
 let senha = "Exemplo123"
 let tentativas = 1 
+// let acertou = false 
 
-while(tentativas <= 3){
+while(tentativas <= 3){ // while(tentivas <=3 && acertou == false)
     let senhaTentavia = prompt("Digite a senha:")
     if(senhaTentavia == senha){
         alert("Usuário logado com sucesso!")
-        break
+        break // ou tentativas = 4 pra encerrar a repetição ou acertou = true
     }else{
         let tentativaSobrando = 3 - tentativas
         alert("Senha incorreta! Você tem mais " + tentativaSobrando + " tentativas.")
