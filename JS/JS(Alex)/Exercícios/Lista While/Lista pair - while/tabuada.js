e883ce0492927722fdc98f3303f922c194327e00
LwBane <emiliepessoarodrigues@gmail.com>
2025-05-05 19:32:52 -0300

    Adicionei ex sobre while

---
+++ JS/JS(Alex)/Exercícios/Lista While/Lista pair - while/tabuada.js
@@ -0,0 +1,10 @@
+let multiplicador = 1
+while(multiplicador <= 10){
+    let multiplicando = 1
+    while(multiplicando <=10){
+        let resultado = multiplicador * multiplicando 
+        alert("Tabuada do : " + multiplicador + "\nMultiplicado por: " + multiplicando + "\nResultado: " + resultado)
+        multiplicando++
+    }
+    multiplicador++
+}
