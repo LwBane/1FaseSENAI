cd179ab26b848ca95ce6de1224b81196bae347eb
LwBane <emiliepessoarodrigues@gmail.com>
2025-05-26 21:37:58 -0300

    Adicionei arquivo de css

---
+++ JS/JS(Alex)/CSS/Fontes - teste/script.js
@@ -0,0 +1,12 @@
+function mostrarFonte1(){
+    document.getElementById('fonte1').style.display = 'flex';
+    document.getElementById('fonte2').style.display = 'none';
+
+}
+
+function mostrarFonte2(){
+
+    document.getElementById('fonte1').style.display = 'none';
+    document.getElementById('fonte2').style.display = 'flex';
+
+}
