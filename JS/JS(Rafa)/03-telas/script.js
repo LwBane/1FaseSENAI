31c110c0149f48896154582ad128c074fe054799
LwBane <emiliepessoarodrigues@gmail.com>
2025-05-29 22:07:47 -0300

    Atualizei o arquivo das telas

--- JS/JS(Rafa)/03-telas/script.js
+++ JS/JS(Rafa)/03-telas/script.js
@@ -1,11 +1,50 @@
+// let nome, email, senha 
+
+let usuario = {
+    nome: "", 
+    email: "", 
+    senha: "" 
+}
+
+    // Objeto para armazenar os dados do usuário - é oq ta dentro do par de chaves -
+    
+function cadastrar(){
+    usuario.nome = document.getElementById('inpCadNome').value  
+    usuario.email = document.getElementById('inpCadEmail').value
+    usuario.senha = document.getElementById('inpCadSenha').value
+    alert('Cadastro realizado com sucesso!')
+
+    console.log(usuario); // Exibe os dados do usuário no console
+    esconderInputs()
+    mostrarLogin()
+}
+
+function logar(){
+
+    let nome = document.getElementById('inpLogNome').value
+    let senha = document.getElementById('inpLogSenha').value
+
+    if(nome === usuario.nome && senha === usuario.senha){
+        alert('Login realizado com sucesso!')
+        esconderInputs()
+        mostrarProdutos()
+    }else{
+        alert('Usuário ou senha inválidos!')
+    }
+
+}
+
+
 function mostrarLogin(){
     esconderTodas()
     document.getElementById('login').style.display = 'flex';
+    document.getElementById('inpLogNome').focus(); // Foca no campo de nome do login
 }
 
 function mostrarCadastro(){
     esconderTodas()
     document.getElementById('cadastro').style.display = 'flex';
+    document.getElementById('inpCadNome').focus(); // Foca no campo de nome do cadastro
 }
 
 function mostrarProdutos(){
@@ -20,6 +59,14 @@ function esconderTodas(){
     // Esconder novas páginas    
 }
 
+function esconderInputs(){
+    document.getElementById('inpCadNome').value = ''
+    document.getElementById('inpCadEmail').value = ''
+    document.getElementById('inpCadSenha').value = ''
+    
+    document.getElementById('inpLogNome').value = ''
+    document.getElementById('inpLogSenha').value = ''
+}
 
 
  
