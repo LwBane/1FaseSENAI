eb71e8ddce7b4e1388247bdd17bd986f303d3817
LwBane <emiliepessoarodrigues@gmail.com>
2025-06-10 20:03:33 -0300

    Explicações sobre o array

---
+++ JS/JS(Alex)/Array/array.js
@@ -0,0 +1,16 @@
+let nomes = ['João', 'Maria']
+
+// console.log(nomes[1])
+// console.log(nomes.length) // pra saber o tamanho do array
+
+// for(let i=0; 9 < nomes.length; i++) {
+//     console.log(nomes[i])
+// }
+
+// nomes[1] = 'José' --> pra mudar o valor de um elemento do array (foi de Maria para José)
+
+// nomes.push('José') --> pra adicionar um elemento no final do array
+// nomes.pop() --> pra remover o último elemento do array
+
+// nomes.unshift('Ana') --> pra adicionar um elemento no início do array
+// nomes.shift() --> pra remover o primeiro elemento do array
