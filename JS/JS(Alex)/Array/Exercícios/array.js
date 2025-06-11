925e4e3e04e28b0c5b89cba366dc919b4314be4e
LwBane <emiliepessoarodrigues@gmail.com>
2025-06-10 22:13:53 -0300

    Add outro jeito de fazer o exercício de inverter

--- JS/JS(Alex)/Array/Exercícios/array.js
+++ JS/JS(Alex)/Array/Exercícios/array.js
@@ -16,9 +16,17 @@ let nomes = ['João', 'Clara', 'Jonas', 'Carla', 'Maria', 'Alessandro']
 let nomesVirgula = nomes.reduce((acumulado, valorAtual) => acumulado + ', ' + valorAtual)
 console.log(nomesVirgula)
 
+// inverter os números do array
+
 let numeros2 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
-let numerosInvertidos = numeros2.map((elemento => elemento)).reverse()
+// let numerosInvertidos = numeros2.map((elemento => elemento)).reverse()
+
+
+   let numerosInvertidos = numeros2.map((elemento, index,) => {
+   let tamanhoArray = numeros2.length - 1
+   return numeros2[tamanhoArray - index] 
+})
 
 console.log(numeros2)
 console.log(numerosInvertidos)
